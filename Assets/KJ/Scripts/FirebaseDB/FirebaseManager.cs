8e732afd4060b3ea9f776fe4ada7ae23db65a050
GameClientDeveloperKimJin <devjin6189@gmail.com>
2026-04-16 23:12:10 +0900

    feat : 로그인 / 회원가입

--- Assets/KJ/Scripts/FirebaseDB/FirebaseManager.cs
+++ Assets/KJ/Scripts/FirebaseDB/FirebaseManager.cs
@@ -2,29 +2,40 @@ using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Firestore;
+using System;
 using UnityEngine;
 
-public class FirebaseManager : MonoBehaviour
+public class FirebaseManager : MonoBehaviour, IDisposable
 {
     public static FirebaseManager Instance { get; private set; }
 
     public FirebaseFirestore Firestore { get; private set; }
     public DatabaseReference RealtimeDB { get; private set; }
-    public FirebaseAuth Auth { get; private set; }
+    public FirebaseAuth Auth { get; set; }
 
-    public bool IsReady { get; private set; } = false;
+    public bool IsConnect { get; private set; } = false;
+
+    public void Dispose()
+    {
+        AuthManager.Instance.SignOut();
+    }
 
     void Awake()
     {
         // ΩÃ±€≈Ê º≥¡§
-        if (Instance != null) { Destroy(gameObject); return; }
+        if (Instance != null) 
+        { 
+            Destroy(gameObject); 
+            return; 
+        }
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
         InitializeFirebase();
     }
 
-    async void InitializeFirebase()
+    private async void InitializeFirebase()
     {
         DependencyStatus status =
             await FirebaseApp.CheckAndFixDependenciesAsync();
@@ -36,7 +47,7 @@ public class FirebaseManager : MonoBehaviour
      .GetInstance("https://legendguardian-d1c51-default-rtdb.firebaseio.com/")
      .GetReference("/");
             Auth = FirebaseAuth.DefaultInstance;
-            IsReady = true;
+            IsConnect = true;
             Debug.Log("Firebase √ ±‚»≠ øœ∑·!");
         }
         else
