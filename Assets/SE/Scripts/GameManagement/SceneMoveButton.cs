f47ef8556a5efe06db367c85a8e6c30241f4387e
kajaq777 <kajaq777@gmail.com>
2026-04-23 00:59:29 +0900

    feat: 랭킹, 맵 Scene 추가

--- Assets/SE/Scripts/GameManagement/SceneMoveButton.cs
+++ Assets/SE/Scripts/GameManagement/SceneMoveButton.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneMoveButton
+public class SceneMoveButton : MonoBehaviour
 {
-    
+    [SerializeField] private string targetSceneName;
+
+    public void MoveToScene()
+    {
+        if (string.IsNullOrWhiteSpace(targetSceneName))
+        {
+            Debug.LogWarning($"{gameObject.name}: 이동할 씬 이름이 비어 있습니다.");
+            return;
+        }
+
+        SceneManager.LoadScene(targetSceneName);
+    }
 }
